b8aceac31792037b55c9f0de7f41b3c050d22e46
Johannes Bremer <jb@hausarzt-mvz-dissen.de>
2025-03-14 13:02:49 +0100

    wildcharm colorscheme

--- init.lua
+++ init.lua
@@ -907,7 +907,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'industry'
+      vim.cmd.colorscheme 'wildcharm'
     end,
   },
 
